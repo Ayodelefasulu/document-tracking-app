9bf2d2b15d4f94ea9ce79a181a45c1617de26800
ayodelefasulu <fasuluayodele@gmail.com>
2024-12-02 11:31:00 +0100

    dbhelper function defined in dbhelper.dart

---
+++ lib/util/dbhelper.dart
@@ -0,0 +1,95 @@
+// ignore_for_file: unused_field, prefer_conditional_assignment, unnecessary_null_comparison, unused_import
+
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
+import '../model/model.dart';
+
+class DbHelper {
+  // Tables
+  static String tblDocs = "docs";
+  // Fields of the 'docs' table.
+  String docId = "id";
+  String docTitle = "title";
+  String docExpiration = "expiration";
+  String fqYear = "fqYear";
+  String fqHalfYear = "fqHalfYear";
+  String fqQuarter = "fqQuarter";
+  String fqMonth = "fqMonth";
+  // Singleton
+  static final DbHelper _dbHelper = DbHelper._internal();
+  // Factory constructor
+  DbHelper._internal();
+  factory DbHelper() {
+    return _dbHelper;
+  }
+  // Database entry point
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db == null) {
+    _db = await initializeDb();
+    }
+    return _db;
+  }
+ // Initialize the database
+  Future<Database> initializeDb() async {
+    Directory d = await getApplicationDocumentsDirectory();
+    String p = "${d.path}/docexpire.db";
+    var db = await openDatabase(p, version: 1, onCreate: _createDb);
+    return db;
+  }
+
+  // Create database table
+  void _createDb(Database db, int version) async {
+    await db.execute(
+    "CREATE TABLE $tblDocs($docId INTEGER PRIMARY KEY, $docTitle TEXT, $docExpiration TEXT, $fqYear INTEGER, $fqHalfYear INTEGER, $fqQuarter INTEGER, $fqMonth INTEGER)"
+    );
+  }
+
+  // Insert a new doc
+  Future<int?> insertDoc(Doc doc) async {
+    int? r;
+    Database db = await this.db;
+
+    try {
+      r = await db.insert(tblDocs, doc.toMap());
+    }
+    catch (e) {
+      debugPrint("insertDoc: $e");
+    }
+    return r;
+  }
+
+  // Get the list of docs.
+  Future<List> getDocs() async {
+    Database db = await this.db;
+    var r = await db.rawQuery(
+      "SELECT * FROM $tblDocs ORDER BY $docExpiration ASC");
+    return r;
+  }
+
+  // Gets a Doc based on the id.
+  Future<List> getDoc(int id) async {
+    Database db = await this.db;
+    var r = await db.rawQuery(
+      "SELECT * FROM $tblDocs WHERE $docId = $id" );
+    return r;
+    }
+    // Gets a Doc based on a String payload
+    Future<List?> getDocFromStr(String payload) async {
+    List<String> p = payload.split("|");
+    if (p.length == 2) {
+      Database db = await this.db;
+      var r = await db.rawQuery(
+        "SELECT * FROM $tblDocs WHERE $docId = ${p[0]} AND $docExpiration = '${p[1]}'" );
+      return r;
+    }
+    else {
+      return null;
+    }
+  }
+
+}
